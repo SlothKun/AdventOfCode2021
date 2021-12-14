f1a4f10da5e10e4d5b155f4cb87f32749da347da
SlothKun <slothkun00@gmail.com>
2021-12-14 22:23:07 +0100

    day 14 part 1

---
+++ day14/mainPart1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+class Polymerization:
+    def __init__(self):
+        self.polymer = ""
+        self.rules = {}
+        self.lettersCount = {}
+
+    def loadData(self):
+        with open("input.txt", 'r') as inputFile:
+            lines = inputFile.readlines()
+            self.polymer = lines[0].rstrip()
+            for line in lines[2:]:
+                data = line.rstrip().split(" -> ")
+                self.rules[data[0]] = data[1]
+
+    def countLetters(self, letter):
+        if letter in self.lettersCount:
+            self.lettersCount[letter] += 1
+        else:
+            self.lettersCount[letter] = 1
+
+    def enforceRules(self):
+        newPolymer = self.polymer
+        for step in range(10):
+            firstChar = self.polymer[0]
+            secondChar = ''
+            marge = 1
+            for index, char in enumerate(self.polymer[1:]):
+                secondChar = char
+                charPair = firstChar + secondChar
+                if self.rules[charPair]:
+                    self.countLetters(self.rules[charPair])
+                    newPolymer = newPolymer[:index+marge] + self.rules[charPair] + newPolymer[index+marge:]
+                    marge += 1
+                firstChar = secondChar
+            self.polymer = newPolymer
+
+    def getPartOneAnswer(self):
+        # Sort dict by value
+        self.lettersCount = sorted(self.lettersCount.items(), key=lambda x:x[1])
+        print(self.lettersCount)
+        print(f"Results : {self.lettersCount[-1][1] - self.lettersCount[0][1]}")
+
+    def main(self):
+        self.loadData()
+        for let in self.polymer:
+            self.countLetters(let)
+        self.enforceRules()
+        self.getPartOneAnswer()
+
+
+if __name__ == '__main__':
+    polymerization = Polymerization()
+    polymerization.main()
